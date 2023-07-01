32638ff8d12932ede39fcdba9ccc33d99abb137b
deysan <ideysan@gmail.com>
2023-07-01 23:46:26 +0300

    Update routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 import { Link } from 'react-router-dom';
-import { Suspense } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from 'app/providers/router';
 
 export function App() {
   const { theme, toggleTheme } = useTheme();
@@ -19,12 +16,7 @@ export function App() {
         <Link to="/about">About</Link>
       </div>
 
-      <Suspense fallback={<h2>Loading...</h2>}>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 }
