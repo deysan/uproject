a284f498bb059f1e6c4dea0624eda794dd58251c
deysan <ideysan@gmail.com>
2023-07-05 00:17:28 +0300

    Fix i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,21 +5,6 @@ import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
-
-function MyComponent() {
-  const { t, i18n } = useTranslation();
-
-  const toggleLang = () => {
-    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
-  };
-
-  return (
-    <div>
-      <button onClick={toggleLang}>lang</button>
-    </div>
-  );
-}
 
 export function App() {
   const { theme } = useTheme();
@@ -28,10 +13,10 @@ export function App() {
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
+
         <div className="content-page">
           <Sidebar />
           <AppRouter />
-          <MyComponent />
         </div>
       </Suspense>
     </div>
