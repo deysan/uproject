77286f99018ed68b2b3a2a1e378d1a8ab62cabc0
deysan <ideysan@gmail.com>
2023-07-08 23:46:23 +0300

    Add not found page

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,18 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import cls from './NotFoundPage.module.scss';
+
+interface NotFoundPageProps {
+  className?: string;
+}
+
+export function NotFoundPage({ className = '' }: NotFoundPageProps) {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(cls.notFoundPage, {}, [className])}>
+      {t('page-not-found')}
+    </div>
+  );
+}
